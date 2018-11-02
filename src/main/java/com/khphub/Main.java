931caaca41b2ed519a2281d33e6ffa6d12e450de
keehyun park <keehyun2@gmail.com>
2018-11-02 10:34:13 +0900

    xx

--- src/main/java/com/khphub/Main.java
+++ src/main/java/com/khphub/Main.java
@@ -3,16 +3,13 @@ package com.khphub;
 import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.dizitart.no2.Document;
-import org.dizitart.no2.Nitrite;
-import org.dizitart.no2.NitriteCollection;
+import org.bson.Document;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -20,6 +17,10 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class Main {
 
@@ -32,6 +33,8 @@ public class Main {
 	static Date date;
 
 	static Logger log = Logger.getLogger("com.gargoylesoftware");
+	
+	static Logger logger = Logger.getLogger("com.khphub");
 
 	public static void main(String[] args) throws Exception {
 
@@ -65,19 +68,18 @@ public class Main {
 		List<WebElement> list_goods = e_driver.findElements(By.className("_itemSection"));
 		System.out.println("상품 수 : " + list_goods.size());
 		
-		Nitrite db = Nitrite.builder().compressed().filePath("/Temp/test.db").openOrCreate("test", "1234");
+//		MongoClient mongoClient = new MongoClient("ds147723.mlab.com", 47723);
+		
+		MongoClientURI uri = new MongoClientURI("mongodb://product1:product1@ds147723.mlab.com:47723/product?authSource=product");
+		MongoClient mongoClient = new MongoClient(uri);
+		logger.info("MongoClient connected");
 
-		// Create a Nitrite Collection
-		NitriteCollection collection = db.getCollection("test");
+		MongoDatabase db = mongoClient.getDatabase("product");
+		logger.info("Get 'namu' MongoDatabase");
+		
+		MongoCollection<Document> documentMongoCollection = db.getCollection("naver");
+		logger.info("Get 'namudb' Document");
 
-		// create a document to populate data
-//		Document doc = Document.createDocument("firstName", "John").put("lastName", "Doe").put("birthDay", new Date())
-//				.put("data", new byte[] { 1, 2, 3 }).put("fruits", new ArrayList<String>() {
-//					{
-//						add("apple");
-//						add("orange");
-//					}
-//				}).put("note", "a quick brown fox jump over the lazy dog");
 		
 		for (WebElement webElement : list_goods) {
 			System.out.println(webElement.findElement(By.className("_productLazyImg")).getAttribute("src"));
