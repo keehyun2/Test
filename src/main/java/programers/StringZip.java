c919f5a4ebe31df3901f5d43f4326173c1432469
박기현 <parkkh@dsg.mz.co.kr>
2020-02-07 17:41:10 +0900

    dd

--- src/main/java/programers/StringZip.java
+++ src/main/java/programers/StringZip.java
@@ -1,5 +1,9 @@
 package programers;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 //문제 설명
 //데이터 처리 전문가가 되고 싶은 어피치는 문자열을 압축하는 방법에 대해 공부를 하고 있습니다. 최근에 대량의 데이터 처리를 위한 간단한 비손실 압축 방법에 대해 공부를 하고 있는데, 문자열에서 같은 값이 연속해서 나타나는 것을 그 문자의 개수와 반복되는 값으로 표현하여 더 짧은 문자열로 줄여서 표현하는 알고리즘을 공부하고 있습니다.
 //간단한 예로 aabbaccc의 경우 2a2ba3c(문자가 반복되지 않아 한번만 나타난 경우 1은 생략함)와 같이 표현할 수 있는데, 이러한 방식은 반복되는 문자가 적은 경우 압축률이 낮다는 단점이 있습니다. 예를 들면, abcabcdede와 같은 문자열은 전혀 압축되지 않습니다. 어피치는 이러한 단점을 해결하기 위해 문자열을 1개 이상의 단위로 잘라서 압축하여 더 짧은 문자열로 표현할 수 있는지 방법을 찾아보려고 합니다.
@@ -47,34 +51,164 @@ package programers;
 //이 경우 어떻게 문자열을 잘라도 압축되지 않으므로 가장 짧은 길이는 17이 됩니다.
 
 // 1개 단위부터 (문자열 length/2)개 단위까지 각각 압축해서 최소값을 출력   
+// https://programmers.co.kr/learn/courses/30/lessons/60057
 
 public class StringZip {
     public int solution(String s) {
-        // s = aabbaccc 1개 단위로 자른경우 2a2ba3c - a2b2ac3
-        int resultLength = 1; // 첫번 째 문자는 무조건 필요 
-        boolean eqFlag = false;
-        for (int i = 0; i < s.length() - 1; i++) {
-            if(s.substring(i, i+1).equals(s.substring(i+1, i+2))) {
-                // i 위치에서 다음문자와 일치하는 경우
-                // 숫자를 붙히고 길이가 +1 됨, 직전 위치에도 일치하는 경우였으면 +1 안함.    
-                if(!eqFlag) {
-                    //System.out.println(123);
-                    resultLength++;
-                    eqFlag = true;
+        //System.out.println(s);
+        int minLength = s.length();
+        for (int unit = 1; unit <= s.length() / 2; unit++) { // for #01
+            String composeStr = "";
+            int repeatCnt = 1;
+            
+            String preStr = "";
+            String postStr = s;
+           
+            for (int i = unit; i < s.length() + 1 ; i+=unit) { // for #02
+                
+                preStr = postStr.substring(0, unit);
+                postStr = postStr.substring(unit); // unit ~ 문자열 끝까지 절단
+//                System.out.println(unit + ", " + preStr + ", " +postStr + ", " + i);
+                
+                if(postStr.startsWith(preStr)) {
+                    // i 위치에서 preStr 와 postStr 앞부분이 일치하는 경우
+                    repeatCnt = repeatCnt + 1; // 반복횟수 1 추가
+                }else {
+                    // i 위치에서 preStr 와 postStr 앞부분이 일치하지 않는 경우 - 1. 문자열 끝에 길이가 안맞는 경우 , 2 문자가 다른경우 
+                    if(repeatCnt > 1) { // 반복횟수가 1보다 큰경우 
+                        composeStr = composeStr + String.valueOf(repeatCnt) + preStr;
+                        repeatCnt = 1;
+                    }else {
+                        composeStr = composeStr + preStr;
+                    }
                 }
-            }else {
-                // i 위치에서 다음문자와 일치하지 않는 경우
-                // 새 문자 추가되고 길이 + 1됨.
-                resultLength++;
-                eqFlag = false;
             }
+            
+            composeStr = composeStr + postStr;
+            
+//            System.out.println(unit + ", "+composeStr);
+            
+            minLength = Math.min(composeStr.length(), minLength);
         }
         
-        return resultLength;
+        return minLength;
+    }
+    
+    public int solution2(String s) {
+        int answer = 0;
+
+        for(int i=1; i<=(s.length()/2)+1; i++){
+            int result = getSplitedLength(s, i, 1).length();
+            answer = i==1 ? result : (answer>result?result:answer);
+        }
+
+        return answer;
+    }
+    
+    /**
+     * 
+     * @param s 문자열
+     * @param n 단위 
+     * @param repeat 반복된 횟수
+     * @return
+     */
+    public String getSplitedLength(String s, int n, int repeat){
+        if(s.length() < n) return s; // 기저 조건
+        String result = "";
+        String preString = s.substring(0, n);
+        String postString = s.substring(n, s.length());
+
+        // 불일치 -> 현재까지 [반복횟수 + 반복문자] 조합
+        if(!postString.startsWith(preString)){
+            if(repeat ==1) return result += preString + getSplitedLength(postString, n, 1); 
+            return result += Integer.toString(repeat) + preString + getSplitedLength(postString, n, 1);
+        }
+
+        return result += getSplitedLength(postString, n, repeat+1);
     }
     
     public static void main(String[] args) {
         
-        System.out.println(new StringZip().solution("aabbaccccvvcccv"));
+        System.out.println(new StringZip().solution("a"));
+        System.out.println(new StringZip().solution("aabbaccc"));                   // 7
+        System.out.println(new StringZip().solution("ababcdcdababcdcd"));           // 9
+        System.out.println(new StringZip().solution("abcabcdede"));                 // 8
+        System.out.println(new StringZip().solution("abcabcabcabcdededededede"));   // 14
+        System.out.println(new StringZip().solution("xababcdcdababcdc"));           // 16
+        System.out.println(new StringZip().solution("aaaaaaaaaa"));                 // 3
+        
+//        System.out.println(new StringZip().getSplitedLength("aaaabbaccc", 2, 1));
+        
+//        System.out.println(new StringZip().numJewelsInStones("ab","aaaabbaccc"));
+    }
+    
+    // https://leetcode.com/problems/jewels-and-stones
+    public int numJewelsInStones(String J, String S) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        
+        for (char key : S.toCharArray()) {
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+        int result = 0;
+        
+        for (char key : J.toCharArray()) {
+            result = result + map.getOrDefault(key, 0);
+        }
+        
+        return result;
+    }
+    
+    int[] lens;
+    String s;
+
+    public int 이효준(String s){
+        if(s.length()==1) return 1;
+        
+        lens=new int[s.length()/2];
+        this.s=s;
+    
+        for(int i=0; i<lens.length; i++){
+            int len = getLen(i+1);
+            lens[i]=len;
+        }
+        
+        Arrays.sort(lens);
+
+        return lens[0];
+    }
+
+    private int getLen(int slice) {
+
+        String sum=""; String counted=""; 
+        if(slice<=s.length())
+            counted=s.substring(0,slice);
+        else{
+            return s.length();
+        }
+
+        int count=0; int i;
+        for(i=0; i<s.length(); i+=slice){
+            String current="";
+            if(i+slice <= s.length()) current=s.substring(i,i+slice);
+            else current=s.substring(i,s.length());
+            
+            if(current.equals(counted)){
+                count++;
+            }else{
+                if(count>1) sum+=count;
+                sum+=counted;
+                counted=current;
+                count=1;
+            }    
+        }
+
+        if(i == s.length()){
+            if(count>1) sum+=count;
+            sum+=counted;
+        }else{
+            sum+=s.substring(i-slice,s.length());
+        }
+
+        return sum.length();
     }
 }
